4a2e6fc3a2a5b675b4d8ead04ca0d8f9947d21cd
Basil Fierz <basil.fierz@hotmail.com>
2020-02-14 22:39:06 +0100

    Allow to strip the file path prefix to create relative paths for BitBucket

--- BitBucketCodeCoverage/bbcov.cpp
+++ BitBucketCodeCoverage/bbcov.cpp
@@ -18,11 +18,20 @@ public:
 		const Plugin::CoverageData& coverageData,
 		const std::optional<std::wstring>& argument) override
 	{
-		std::filesystem::path output = argument ? *argument : L"BitBucketCodeCoverage.json";
+		std::filesystem::path output = L"BitBucketCodeCoverage.json";
+		std::filesystem::path prefix = L"";
+		if (argument)
+		{
+			const auto separator = argument->find_first_of(L'?');
+			if (separator != std::wstring::npos)
+				output = argument->substr(separator + 1);
+			prefix = argument->substr(0, separator);
+		}
+
 		std::wofstream ofs{ output };
 
 		if (!ofs)
-			throw std::runtime_error("Cannot create the output file for SimpleExport");
+			throw std::runtime_error("Cannot create the output file for BitBucketCodeCoverage");
 
 		// Open the json structure
 		ofs << LR"({)" << "\n";
@@ -53,9 +62,13 @@ public:
 					[](const auto& line) { return line.HasBeenExecuted(); }
 				);
 
-				ofs << LR"(    "path": ")" << (*file)->GetPath().filename().wstring() << "\",\n";
+				// Strip the prefix in order to prepare the files paths for BitBucket.
+				// File paths need to be relative to the root.
+				const auto path = std::filesystem::relative((*file)->GetPath(), prefix);
+				ofs << LR"(    "path": ")" << path.generic_wstring() << "\",\n";
 				ofs << LR"(    "coverage": ")";
 
+				// Print fully covered lines
 				if (std::begin(lines) != separator)
 				{
 					ofs << LR"(C:)";
@@ -66,6 +79,11 @@ public:
 							ofs << ",";
 					}
 				}
+
+				// Unclear how to determine partially covered lines
+				//ofs << LR"(P:)";
+
+				// Print fully uncovered lines
 				if (separator != std::end(lines))
 				{
 					if (std::begin(lines) != separator)
@@ -97,14 +115,22 @@ public:
 
 	void CheckArgument(const std::optional<std::wstring>& argument) override
 	{
-		// Try to check if the argument is a file.
-		if (argument && !std::filesystem::path{ *argument }.has_filename())
-			throw Plugin::OptionsParserException("Invalid argument for BitBucketCodeCoverage export.");
+		// The parameter for this plugin contains an optional and non-optional part.
+		// The first part is mandatory, in order to create relative paths for BitBucket Server.
+		// The optional part contains the output file name.
+		if (argument)
+		{
+			if (const auto separator = argument->find_first_of(L'?'); separator != std::wstring::npos)
+			{
+				if (!std::filesystem::path{ argument->substr(separator + 1) }.has_filename())
+					throw Plugin::OptionsParserException("Invalid argument for BitBucketCodeCoverage export.");
+			}
+		}
 	}
 
 	std::wstring GetArgumentHelpDescription() override
 	{
-		return L"output file (optional)";
+		return L"<prefix>?<output file (optional)>";
 	}
 
 	int GetExportPluginVersion() const override
